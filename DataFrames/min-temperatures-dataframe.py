c32b11c4a5ff80f24aaeee2474010721a503e7be
Sai Sreekar Godala <saisreekargodala@Sais-MacBook-Pro.local>
2023-10-06 20:34:16 +0530

    feat: sparksql codes commit

---
+++ DataFrames/min-temperatures-dataframe.py
@@ -0,0 +1,43 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+
+spark = SparkSession.builder.appName("MinTemperatures").getOrCreate()
+
+schema = StructType([ \
+                     StructField("stationID", StringType(), True), \
+                     StructField("date", IntegerType(), True), \
+                     StructField("measure_type", StringType(), True), \
+                     StructField("temperature", FloatType(), True)])
+
+# // Read the file as dataframe
+df = spark.read.schema(schema).csv("./../assets/1800.csv")
+df.printSchema()
+
+# Filter out all but TMIN entries
+minTemps = df.filter(df.measure_type == "TMIN")
+
+# Select only stationID and temperature
+stationTemps = minTemps.select("stationID", "temperature")
+
+# Aggregate to find minimum temperature for every station
+# minTempsByStation = stationTemps.groupBy("stationID").min("temperature")
+minTempsByStation = stationTemps.groupBy("stationID").agg(func.min("temperature").alias("temperature_min"))
+minTempsByStation.show()
+
+# Convert temperature to fahrenheit and sort the dataset
+minTempsByStationF = minTempsByStation.withColumn("temperature_min_farenheit",
+                                                  func.round(func.col("temperature_min") * 0.1 * (9.0 / 5.0) + 32.0, 2))\
+                                                  .select("stationID", "temperature_min_farenheit")\
+                                                  .sort("temperature_min_farenheit")
+minTempsByStationF.show()
+                                                  
+# Collect, format, and print the results
+results = minTempsByStationF.collect()
+
+for result in results:
+    print(result[0] + "\t{:.2f}F".format(result[1]))
+    
+spark.stop()
+
+                                                  
